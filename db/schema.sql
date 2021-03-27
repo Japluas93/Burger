297f4bc6a62c3a61fd173e888bc0990692f8b7f5
Julian Pluas <julianpluas@gmail>
2021-03-27 12:29:00 -0700

    Created the burgers database

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+-- Drops the "burgers_db" if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+-- Creates the "burgers_db" database --
+CREATE database burgers_db;
+-- Makes it so all of the following code will affect "burgers_db" --
+USE burgers_db;
