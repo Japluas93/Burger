a03a86df858cf0926eb3dbfb9a5fc912dc3770ef
Julian Pluas <julianpluas@gmail>
2021-03-27 12:36:18 -0700

    Created a burgers table

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,15 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE database burgers_db;
 -- Makes it so all of the following code will affect "burgers_db" --
 USE burgers_db;
+
+-- Table "burgers"--
+-- "id" is a numeric column that cannot contain null and will automatically increment as new rows are created. --
+-- "burger_name" is a string column which cannot contain null --
+-- "devoured" is a boolean column --
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false, 
+    PRIMARY KEY (id)
+);
