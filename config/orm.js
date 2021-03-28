01fe768d2afc29cf7febbe4f9c888a9282c8acf3
Julian Pluas <julianpluas@gmail>
2021-03-28 11:22:25 -0700

    Added helper function to convert question marks into strings

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,17 @@
 // Set up my connection
 const connection = require("./connection.js");
 
+// Helper function to loop through and create array of question marks and turn it into a string
+function printQuestionMarks(num) {
+  const arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
 const orm = {
   selectAll: function (tableInput, cb) {
     const query = "SELECT * FROM " + tableInput + ";";
