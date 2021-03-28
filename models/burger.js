540c6ea49859e85157c7bdf33efcf0a15dfe45d1
Julian Pluas <julianpluas@gmail>
2021-03-28 12:27:03 -0700

    Added my selectAll function

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,9 @@
 // Sets up my connection to orm
 const orm = require("../config/orm.js");
-const burger = {};
+const burger = {
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
+    });
+  },
+};
