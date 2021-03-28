1cde7fd6496efd79f7286444b78e561717d5cbe7
Julian Pluas <julianpluas@gmail>
2021-03-28 13:14:48 -0700

    Setting handlebars for the express app

--- server.js
+++ server.js
@@ -2,3 +2,8 @@
 const express = require("express");
 // Creates my express application
 const app = express();
+// Set Handlebars.
+var expressHandleBars = require("express-handlebars");
+
+app.engine("handlebars", expressHandleBars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
