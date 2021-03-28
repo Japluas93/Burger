2a06ba94d9f61b6a9c0d63cd5d0a5bafb8fd4be2
Julian Pluas <julianpluas@gmail>
2021-03-28 12:45:53 -0700

    Create router for the application

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,5 @@
 var express = require("express");
 // Sets up the connection to burger js
 var burger = require("../models/burger.js");
+// Creates router for the application
+var router = express.Router();
