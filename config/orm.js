01f3a61b1f499a26db3f1a85006242d2d8bb0229
Julian Pluas <julianpluas@gmail>
2021-03-28 11:18:25 -0700

    Added my selectAll method

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,17 @@
 // Set up my connection
 const connection = require("./connection.js");
 
-const orm = {};
+const orm = {
+  selectAll: function (tableInput, cb) {
+    const query = "SELECT * FROM " + tableInput + ";";
+    connection.query(query, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+};
 
 // Exporting the orm object
 module.exports = orm;
