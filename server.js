bb5930cb9888deb2522bbe5c0365d98421c9b539
Julian Pluas <julianpluas@gmail>
2021-03-29 14:38:03 -0700

    Import the routes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // Loads my express module
 const express = require("express");
-// Creates my express application
+// Creates my express application.
 const app = express();
-// Set Handlebars.
+// Set Handlebars
 var expressHandleBars = require("express-handlebars");
-// App will serve static content from the "public" folder.
+// Import routes to the server.
+const routes = require("./controllers/burgers_controllers.js");
+// App will serve static content from the "public" folder
 app.use(express.static("public"));
 
 app.engine("handlebars", expressHandleBars({ defaultLayout: "main" }));
